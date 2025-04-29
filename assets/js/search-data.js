bf30bbb959ba649f7de0644073ab6bc0307222ad
SergioLozano <sergio.lozano.alvarez@gmail.com>
2025-04-29 08:28:54 +0000

    Deploying to gh-pages from @ SergioLozano/sergiolozano.github.io@f5e2173b798f34d204d81b3c720b451175a88a4e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,7 +451,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "projects-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/10_project/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
