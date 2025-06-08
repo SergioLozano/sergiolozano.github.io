fc86a221c78ba628aad904812142149e4609f7a7
SergioLozano <sergio.lozano.alvarez@gmail.com>
2025-06-08 10:29:23 +0000

    Deploying to gh-pages from @ SergioLozano/sergiolozano.github.io@248442e85afb5a237df9bda397199fd351f3c04b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Here you can get a glimpse into my professional career, including a summary my skills, experience, and interests.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
