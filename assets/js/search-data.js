7978e2d3f7866cc17503482aa92e11b473ec1a9a
SergioLozano <sergio.lozano.alvarez@gmail.com>
2025-06-09 15:51:11 +0000

    Deploying to gh-pages from @ SergioLozano/sergiolozano.github.io@517989d9cd47ec31fc48ecac985a786264fd27b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,21 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-how-we-migrated-debitoor-to-sumup",
+        },{id: "post-how-we-migrated-debitoor-to-sumup",
         
           title: "How we migrated Debitoor to SumUp",
         
